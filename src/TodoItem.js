0a0fe127522e50e75f8996d2642c72c4a66afc16
Hanny <Hannylinares@gmail.com>
2023-08-27 17:24:52 -0300

    Cambio en los estilos de TodoItem

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,12 +1,16 @@
 import'./TodoItem.css';
-import {BsCircle} from 'react-icons/bs';
+import {RiCheckboxBlankCircleFill} from 'react-icons/ri';
 import {RiCloseLine} from 'react-icons/ri';
+import {FaCheck} from 'react-icons/fa';
 
 function TodoItem(props) {
     return (
         <li className='TodoItem'>
-          <i className={`Icon icon-check ${props.completed && 'icon-check--active'}`}>
-            <BsCircle/>
+          <i className={`Icon icon-UnCheck ${props.completed && 'icon-uncheck--active'}`}>
+            <RiCheckboxBlankCircleFill/>
+          </i> 
+          <i className={`Icon icon-Check ${props.completed && 'icon-Check--active'}`}>
+            <FaCheck/>
           </i> 
           <p className={`TodoItem-p ${props.completed && 'TodoItem-p--completed'}`}>
             {props.text}
