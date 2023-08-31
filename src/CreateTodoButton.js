f38a3b3d86a064bc5b30500da374a491ef11c260
Hanny Linares <hannylinares@gmail.com>
2023-08-31 19:12:08 -0300

    actualizacion

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -6,7 +6,17 @@ function CreateTodoButton() {
     return (
         <div className='CreateTodo'>
             <h3>Create new To-Do</h3>
-            <button className='CreateTodo-button'><BsFillFileEarmarkPlusFill size='32px' color='#44cac6'/></button>
+            <button 
+                className='Icon CreateTodoButton' 
+                onClick={
+                    (event) => {
+                        console.log('le diste click');
+                        console.log(event);
+                        console.log(event.target);
+                    }
+                }
+                ><BsFillFileEarmarkPlusFill/>
+            </button>
         </div>
     )
 }
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,11 +1,29 @@
+import React from 'react';
 import'./TodoSearch.css';
 import {BiSearchAlt} from 'react-icons/bi';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     return (
         <div className='Search'>
-            <input className='Search-bar'placeholder="Cortar cebolla..."/>
-            <button className='Search-icon'><BiSearchAlt size='25px' color='#44cac6'/></button>
+            <input 
+                className='Search-bar'
+                placeholder='Cortar cebolla...'
+                value={searchValue}
+                onChange={(event) => {
+                    setSearchValue(event.target.value);
+                }}
+            />
+            <button className='Search-icon'
+                onClick={
+                    (event) => {
+                        console.log('Diste click a search');
+                        console.log(event.target);
+                    }
+                }
+                ><BiSearchAlt/>
+            </button>
         </div>
     );
 }
